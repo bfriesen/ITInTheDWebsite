e7657e335fda94f0945bc652feb3c490da27d240
MI\v-PPadonou <MI\v-PPadonou>
2012-11-17 18:27:38 -0500

    This Is philemon Homepage with. Don't worry about the language. It is french.

---
+++ ITinTheDWebSite/Content/AboutpageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITinTheDWebSite.Content
+{
+    public class AboutpageController : Controller
+    {
+        //
+        // GET: /Aboutpage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
