81227eeb31f3e61e6417eb2a6bd83f5f5f25ab71
MI\v-AAziz <MI\v-AAziz>
2012-11-18 15:22:28 -0500

    Fixed the HomeController.
    - It was called HomepageController but now HomeController.
    
    - Under Views the Homepage was changed to Home.

--- ITinTheDWebSite/Controllers/HomeController.cs
+++ ITinTheDWebSite/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace ITinTheDWebSite.Controllers
 {
-    public class HomepageController : Controller
+    public class HomeController : Controller
     {
         //
         // GET: /Homepage/
