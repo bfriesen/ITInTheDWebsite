8b20240d86c9366c1f54bab4a4d3bd3454715d44
MI\v-AAziz <MI\v-AAziz>
2012-11-19 13:00:15 -0500

    Fixes:
    - Separate Home button.
    - Separate About button.
    - Drop Youtube Video.
    - Bootstrap with MVC4.
    - Clean up to the previous solution.
    
    Not complete:
    
    - Drop YouTube video to be automatically sized.

--- ITinTheDWebSite/App_Start/BundleConfig.cs
+++ ITinTheDWebSite/App_Start/BundleConfig.cs
@@ -25,6 +25,16 @@ namespace ITinTheDWebSite
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap*"));
+            bundles.Add(new StyleBundle("~/Content/bootswatch").Include(
+                    "~/Content/variables.less*",
+                    "~/Content/bootswatch.less"
+                    ));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*"));
+
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
