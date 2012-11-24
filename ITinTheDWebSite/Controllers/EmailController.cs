ba2f627979486c580bc87fbc7f71567c8f795e12
MI\v-PBains <MI\v-PBains>
2012-11-24 12:52:51 -0500

    Added the "Report Error" button in the footer of the _layout.cshtml file plus the code to run it.

---
+++ ITinTheDWebSite/Controllers/EmailController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using sbContact.Models;
+using System.Configuration;
+
+
+
+namespace sbContact.Controllers
+{
+    public class EmailController : Controller
+    {
+        //
+        // GET: /Email/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Sendit(EmailFields Mailer)
+        {
+
+            MailMessage email = new MailMessage
+            {
+                Sender = new MailAddress(Mailer.Email),
+                Subject = Mailer.Subject,
+                Body = Mailer.Message
+            };
+            email.To.Add(new MailAddress("whover.@whatver.com"));
+            //email.To.Add(new System.Configuration.ConfigurationManager.AppSettings["EmailTo"].ToString());
+            SmtpClient client = new SmtpClient();
+            //client.Credentials = new NetworkCredential(username, password);
+            client.Send(email);
+            return View();
+        }
+
+    }
+}
---
+++ ITinTheDWebSite/Models/EmailFields.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace sbContact.Models
+{
+    public class EmailFields
+    {
+
+        [Required]
+        [StringLength(100)]
+        [Display(Order = 1)]
+        public virtual string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(254)]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*")]
+        [Display(Order = 2)]
+        public virtual string Email { get; set; }
+
+        [Required]
+        [StringLength(254)]
+        [Display(Order = 3)]
+        public virtual string Subject { get; set; }
+
+        [Required]
+        [StringLength(2000)]
+        [DataType(DataType.MultilineText)]
+        [Display(Order = 4)]
+        public virtual string Message { get; set; }
+    }
+}
