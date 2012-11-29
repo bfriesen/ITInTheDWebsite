62e9b2cc89ae50641c1bd39f05f97903581f1984
MI\v-JMartel <MI\v-JMartel>
2012-11-28 21:46:52 -0500

    Added Curriculum controller

---
+++ ITinTheDWebSite/Controllers/CurriculumController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITinTheDWebSite.Controllers
+{
+    public class CurriculumController : Controller
+    {
+        //
+        // GET: /Curriculum/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
