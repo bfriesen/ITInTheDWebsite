776b8484f3e95976b2a2d44aeb8333d461b850f1
MI\v-PPadonou <MI\v-PPadonou>
2012-12-02 18:22:31 -0500

    Played with the application form a little. Once user login, they will have "Apply" on top of screen. Once they click on apply, the page application form will load. Still working on database connection and resume uploading.

--- ITinTheDWebSite/Controllers/HomeController.cs
+++ ITinTheDWebSite/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ITinTheDWebSite.Models;
 
 namespace ITinTheDWebSite.Controllers
 {
@@ -14,6 +15,12 @@ namespace ITinTheDWebSite.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Application()
+        {
+            Application app = new Application();
+            app.LastName = Request.Form["lastname"];
+            return View(app);
         }
 
     }
